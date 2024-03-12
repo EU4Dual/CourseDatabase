1560adc01e0572c2916691493bd725f0e279f534
Kathy Chan <kathy.chan@dhbw.de>
2024-03-12 15:06:24 +0100

    added api connection in html

--- hello.py
+++ hello.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template, jsonify, request
+import configparser
 import psycopg2
 import json
 
 app = Flask(__name__)
+app.json.sort_keys = False
 
-database = 'test-db'
-user = 'postgres'
-password = 'postgres'
-host = 'localhost'
-port = 5432
+# Create a ConfigParser object
+config = configparser.ConfigParser()
+
+# Read the file using the object
+config.read('config.ini')
+
+# Obtain the configuration values
+database = config.get('database', 'database')
+user = config.get('database', 'user')
+password = config.get('database', 'password')
+host = config.get('database', 'host')
+port = config.get('database', 'port')
 
 # Establish the connection
 connection = psycopg2.connect(database=database, user=user, password=password, host=host, port=port)
@@ -22,24 +31,24 @@ cursor.execute("SELECT * FROM campus;")
 # Retrieve query results
 records = cursor.fetchall()
 
-# Check if there are any tables
-if not records:
-    print("No tables found in the database.")
-else:
-    for name in records:
-        print(name)
-
-
 @app.route('/')
 def hello():
     return 'Hello, World!'
 
 
-@app.route('/index')
+@app.route('/sample-table')
 def index():
-    return render_template('index.html', mycontent=records)
+    return render_template('sample-table-api.html')
 
 
-@app.route('/test-api', methods=['GET'])
-def getCampusName():
-    return jsonify(records)
+@app.route('/getAllData', methods=['GET'])
+def getAllData():
+    # Get column names from cursor description
+    column_names = [desc[0] for desc in cursor.description]
+
+    # Build a list of dictionaries with column headers and values
+    result_data = []
+    for row in records:
+        result_data.append(dict(zip(column_names, row)))
+
+    return jsonify(result_data)
